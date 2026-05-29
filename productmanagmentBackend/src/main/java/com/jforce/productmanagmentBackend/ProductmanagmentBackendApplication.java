6273c6cbbf74a25a3ff220bae0002be04eaa10e2
Inzamamul Shaikh <45431765+InzamamShaikh567@users.noreply.github.com>
2026-05-29 17:30:45 +0530

    Add files via upload

---
+++ productmanagmentBackend/src/main/java/com/jforce/productmanagmentBackend/ProductmanagmentBackendApplication.java
@@ -0,0 +1,12 @@
+package com.jforce.productmanagmentBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductmanagmentBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductmanagmentBackendApplication.class, args);
+	}
+}
